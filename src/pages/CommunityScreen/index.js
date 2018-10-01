9ca0649c26b4ed26da5d58c8ce5cee5b5b1a3742
Unknown <jdsantos@yondu.com>
2018-10-01 19:00:43 +0800

    add images

--- src/pages/CommunityScreen/index.js
+++ src/pages/CommunityScreen/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import { View, Text } from 'react-native'; 
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import MainWrapper from "../../components/MainWrapper";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Icon,
+  List,
+  ListItem,
+  Text,
+  Left,
+  Right,
+  Body,
+  StyleProvider,
+  Input,
+  Label,
+  Form,
+  Item,
+  Textarea,
+  Footer,
+  View
+} from "native-base";
 
+const datas = ["INQUERIES", "COMPLAINS", "REQUEST", "WARRANTY"];
 
-
-export default class CommunityScreen extends React.Component {
+class CommunityScreen extends Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
-    return <View>
-      <Text>CommunityScreen</Text>
-    </View>
+    return (
+      <MainWrapper>
+        <Container style={styles.container}>
+          <Header noShadow transparent>
+            <Left style={{ flex: 1 }}>
+              <Button
+                transparent
+                onPress={() => this.props.navigation.goBack()}
+              >
+                <Icon name="ios-arrow-back" style={{color: '#000'}} />
+              </Button>
+            </Left>
+            <Body style={{ flex: 5 }}>
+              <Title style={{ alignSelf: "center", color: '#000' }}>INQUERIES</Title>
+            </Body>
+            <Right style={{ flex: 1 }} />
+          </Header>
+
+          <Content>
+            
+          </Content>
+          <Button
+            block
+            large 
+            style={{ marginLeft: 20, marginRight: 20, marginBottom: 20 }}
+          >
+            <Text>JOIN NOW</Text>
+          </Button>
+        </Container>
+      </MainWrapper>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFF"
+  }
+});
+
+export default CommunityScreen;
