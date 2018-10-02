30d9479fd9d51514b51f6dec3cec5c627937e18a
Unknown <jdsantos@yondu.com>
2018-10-02 11:27:35 +0800

    add title page

--- src/pages/TermsScreen/index.js
+++ src/pages/TermsScreen/index.js
@@ -24,7 +24,6 @@ import {
   Footer,
   View,
   CheckBox
-
 } from "native-base";
 
 const datas = ["INQUERIES", "COMPLAINS", "REQUEST", "WARRANTY"];
@@ -37,31 +36,97 @@ class TermsScreen extends Component {
     return (
       <MainWrapper>
         <Container style={styles.container}>
-         
+          <Content style={{ padding: 20 }}>
+            <Text
+              style={{
+                color: "#B4C5E7",
+                fontSize: 24,
+                fontWeight: "bold",
+                marginBottom: 20,
+                paddingTop: 30
+              }}
+            >
+              TERMS AND CONDITION
+            </Text>
+            <Text style={{ marginBottom: 20 }}>
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus id quod
+              maxime placeat facere possimus, omnis voluptas assumenda est,
+              omnis dolor repellendus. Temporibus autem quibusdam et aut
+              officiis debitis aut rerum necessitatibus saepe eveniet ut et
+              voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat Et harum quidem rerum facilis est et expedita
+              distinctio. Nam libero tempore, cum soluta nobis est eligendi
+              optio cumque nihil impedit quo minus id quod maxime placeat facere
+              possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+              Temporibus autem quibusdam et aut officiis debitis aut rerum
+              necessitatibus saepe eveniet ut et voluptates repudiandae sint et
+              molestiae non recusandae. Itaque earum rerum hic tenetur a
+              sapiente delectus, ut aut reiciendis voluptatibus maiores alias
+              consequatur aut perferendis doloribus asperiores repellat At vero
+              eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+              praesentium voluptatum deleniti atque corrupti quos dolores et
+              quas molestias excepturi sint occaecati cupiditate non provident,
+              similique sunt in culpa qui officia deserunt mollitia animi, id
+              est laborum et dolorum fuga. Et harum quidem rerum facilis est et
+              expedita distinctio. Nam libero tempore, cum soluta nobis est
+              eligendi optio cumque nihil impedit quo minus id quod maxime
+              placeat facere possimus, omnis voluptas assumenda est, omnis dolor
+              repellendus. Temporibus autem quibusdam et aut officiis debitis
+              aut rerum necessitatibus saepe eveniet ut et voluptates
+              repudiandae sint et molestiae non recusandae. Itaque earum rerum
+              hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+              maiores alias consequatur aut perferendis doloribus asperiores
+              repellat
+            </Text>
 
-          <Content style={{padding: 20}}>
-
-          <Text style={{marginBottom: 20}}>
-At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat 
-          </Text>
-
-          <Text style={{marginBottom: 20}}>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
-          </Text>
-           <Body style={{ flex: 1, flexDirection: "row" , paddingTop: 40,  paddingBottom: 80}}>
-                      <CheckBox
-                     
-                        style={{ marginRight: 20 }}
-                      />
-                      <Text style={{fontWeight: 'bold'}}>I AGREE WITH THE TERMS & CONDITIONS</Text>
-                    </Body>
+            <Text style={{ marginBottom: 20 }}>
+              Et harum quidem rerum facilis est et expedita distinctio. Nam
+              libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat facere possimus, omnis
+              voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+              quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+              eveniet ut et voluptates repudiandae sint et molestiae non
+              recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
+              aut reiciendis voluptatibus maiores alias consequatur aut
+              perferendis doloribus asperiores repellat Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus id quod
+              maxime placeat facere possimus, omnis voluptas assumenda est,
+              omnis dolor repellendus. Temporibus autem quibusdam et aut
+              officiis debitis aut rerum necessitatibus saepe eveniet ut et
+              voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat
+            </Text>
+            <Body
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                paddingTop: 40,
+                paddingBottom: 80
+              }}
+            >
+              <CheckBox style={{ marginRight: 20 }} />
+              <Text style={{ fontWeight: "bold" }}>
+                I AGREE WITH THE TERMS & CONDITIONS
+              </Text>
+            </Body>
           </Content>
           <Button
             full
-          
             style={{ paddingTop: 30, paddingBottom: 30 }}
             onPress={() => this.props.navigation.navigate("Video")}
           >
-            <Text style={{fontWeight: 'bold'}}>AGREE</Text>
+            <Text style={{ fontWeight: "bold" }}>AGREE</Text>
           </Button>
         </Container>
       </MainWrapper>
